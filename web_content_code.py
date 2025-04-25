0246631b376ddfa0c5dacc3727e881433809d79f
sree1975-cyber <sreeduba@hotmail.com>
2025-04-24 23:28:36 -0400

    Update web_content_code.py

--- web_content_code.py
+++ web_content_code.py
@@ -18,7 +18,7 @@ import streamlit as st
 import sqlite3
 from datetime import datetime
 import requests
-from BeautifulSoup4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import numpy as np
 from PIL import Image
 import io
